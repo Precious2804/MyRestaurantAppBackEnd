23e94cce2762452e1f7b5d0aa3e16db35cd90f0b
PreshDev <anipreciousebuka@gmail.com>
2021-08-31 14:44:32 +0100

    jbjfv

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->uuid('unique_id');
             $table->string('first_name')->nullable();
             $table->string('last_name')->nullable();
-            $table->string('email');
+            $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('isAdmin')->default(0);
